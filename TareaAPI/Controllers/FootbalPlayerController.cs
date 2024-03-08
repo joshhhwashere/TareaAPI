94352a626493d74f6b252577c0f0d675ac5bd628
Galilea Gonzalez <Galilea Gonzalez@LAPTOP-MP08L8QN>
2024-03-08 13:59:14 -0500

    Add project files.

---
+++ TareaAPI/Controllers/FootbalPlayerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TareaAPI.Models;
+
+namespace TareaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecaudacionController : ControllerBase
+    {
+        private static List<Cliente> _clientes = new List<Cliente>
+        {
+            new Cliente { ID = 1, Nombre = "Juan", MontoPendiente = 100.50M },
+            new Cliente { ID = 2, Nombre = "Maria", MontoPendiente = 50.20M },
+        };
+
+        [HttpGet]
+        public IEnumerable<Cliente> GetAllClientes()
+        {
+            return _clientes;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetClienteById(int id)
+        {
+            var cliente = _clientes.FirstOrDefault(c => c.ID == id);
+
+            if (cliente != null)
+                return Ok(cliente.MontoPendiente);
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ TareaAPI/Models/FootbalPlayer.cs
@@ -0,0 +1,9 @@
+﻿namespace TareaAPI.Models
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string? Nombre { get; set; } // '?' para hacer la propiedad anulable
+        public decimal MontoPendiente { get; set; }
+    }
+}
